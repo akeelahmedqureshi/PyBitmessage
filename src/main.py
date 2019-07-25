3d88c1e683c585975511e320f23bfb86aadce9f4
Mohit M <mohit.m@cisinlabs.com>
2019-07-25 20:13:34 +0530

    print for testing

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ import state
 if __name__ == '__main__':
     state.kivy = True
     print("Kivy Loading for PyBitmessage......")
+    print("Testing")
     from bitmessagemain import main
     main()
